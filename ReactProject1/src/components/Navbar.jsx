c10ff0ae2a6ee5fd36d8b2f7ebde69e91d381731
liamonk <61438758+liamonk@users.noreply.github.com>
2023-05-18 11:56:03 +1200

    finished react facts page

--- ReactProject1/src/components/Navbar.jsx
+++ ReactProject1/src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import reactLogo from '../assets/React-icon.png'
 
 export default function Navbar (){
     return(
-        <h1>Navbar Goes Here!</h1>
+        <nav>
+            <img className='nav--logo' src={reactLogo}/>
+            <div className='nav--title'>ReactFacts</div>
+            <div className='nav--text'>React Course - Project 1</div>
+        </nav>
     )
 }
